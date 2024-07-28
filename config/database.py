4b2ae38cacbc04a8a333580f319c219dfe01f0ce
Karla038 <karladk038@gmail.com>
2024-07-27 19:44:06 -0600

    Kmeans

--- config/database.py
+++ config/database.py
@@ -1,9 +1,9 @@
-# from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData
 
-# # UrlBD Mysql
-# engine = create_engine("mysql+pymysql://root:password@localhost:3306/nombreBD")
+# UrlBD Mysql
+engine = create_engine("mysql+pymysql://root:password@localhost:3306/nombreBD")
 
-# conexion = engine.connect()
+conexion = engine.connect()
 
 
 
